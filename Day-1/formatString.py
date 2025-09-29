30ca04fd517ea3ec23f01a9d9a7f0772edfd80cc
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-09-29 14:00:55 +0545

    Fix string concatenation error in formatString.py and add comments for using format() and F-strings

---
+++ Day-1/formatString.py
@@ -0,0 +1,10 @@
+#String formatting
+
+age = 30
+o = "my name is Alish , i am " + age
+print(o)
+
+#throws error as string and integer cannot be concatenated
+
+#so we use format() method or F-strings
+
