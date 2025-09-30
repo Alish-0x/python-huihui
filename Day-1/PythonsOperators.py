d3926f76c38bdbfd8ce78407ce5f353400140071
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-09-30 20:44:53 +0545

    Add initial implementation of PythonsOperators.py to demonstrate various Python operators

---
+++ Day-1/PythonsOperators.py
@@ -0,0 +1,72 @@
+#Operators
+#we use + operator to add two values 
+print(3 + 5)
+
+#we use - operator to subtract two values
+print(10 - 2)
+
+#we use * operator to multiply two values
+print(4 * 7)
+#we use / operator to divide two values
+print(20 / 4)
+
+#we use % operator to get the remainder of a division
+print(10 % 3)   
+
+#we use // operator to get the quotient of a division
+print(10 // 3)
+#we use ** operator to get the power of a number
+print(2 ** 3)
+
+#we use == operator to check if two values are equal
+print(5 == 5)
+
+#we use != operator to check if two values are not equal
+print(5 != 3)   
+#we use > operator to check if a value is greater than another value
+print(7 > 4)
+#we use < operator to check if a value is less than another value
+print(3 < 6)
+
+#we use >= operator to check if a value is greater than or equal to another value
+print(5 >= 5)
+
+#we use <= operator to check if a value is less than or equal to another value
+print(4 <= 6)
+
+#we use and operator to check if two conditions are true
+print(5 > 3 and 4 < 6)
+
+#we use or operator to check if at least one condition is true
+print(5 > 3 or 4 > 6)
+
+#we use not operator to reverse the result of a condition
+print(not(5 > 3))
+
+#we use in operator to check if a value is present in a sequence
+print('a' in 'apple')   
+
+
+
+
+#table showing the precedence of Python operators (highest to lowest)
+"""
++---------------------+-----------------------------+
+| Precedence Level    | Operators                   |
++---------------------+-----------------------------+
+| 1 (Highest)         | ()                          |
+| 2                   | **                          |
+| 3                   | +x, -x, ~x                  |
+| 4                   | *, /, //, %                 |
+| 5                   | +, -                        |
+| 6                   | <<, >>                      |
+| 7                   | &                           |
+| 8                   | ^                           |
+| 9                   | |                           |
+| 10                  | ==, !=, >, >=, <, <=, is,   |
+|                     | is not, in, not in          |
+| 11                  | not                         |
+| 12                  | and                         |
+| 13 (Lowest)         | or                          |
++---------------------+-----------------------------+
+"""
