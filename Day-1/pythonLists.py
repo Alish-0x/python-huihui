7d7f34f7d7521b3174c407e35461f7cbb69361ad
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-09-30 20:55:05 +0545

    Add initial implementation of pythonLists.py to demonstrate list usage in Python

---
+++ Day-1/pythonLists.py
@@ -0,0 +1,22 @@
+# Lists - used to store multiple items in a single variable 
+#its created using square brackets
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#just like arrays - it can hold different data types and are indexed from 0 and so on
+
+thislist = ["apple", 1, 2.5, True]
+print(thislist)
+
+#list items are ordered, changeable, and allow duplicate values.
+
+thisxlist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thisxlist)
+
+#we can use the len() and type() function to determine how many items a list has and data type of list just like we used in strings
+print(len(thisxlist))
+print(type(thisxlist))
+
+
+
+
