bb761ff738b4ff9fb0205e99dc053417f97ea4b8
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-01 20:45:43 +0545

    Add looptuples.py to demonstrate various looping techniques with tuples in Python

---
+++ Day-3/looptuples.py
@@ -0,0 +1,18 @@
+# looping through a tuple
+fruits = ("apple", "banana", "cherry")
+for fruit in fruits:
+    print(fruit)
+
+
+# looping through the index of a tuple
+fruits = ("apple", "banana", "cherry")
+for i in range(len(fruits)):
+    print(fruits[i])
+
+# using while loop to iterate through a tuple
+fruits = ("apple", "banana", "cherry")
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+
