28011e58264b7c8a5f755623e1a5398b89f54e7e
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-01 20:06:47 +0545

    Add detailed examples for looping through a list in looplist.py

--- Day-2/looplist.py
+++ Day-2/looplist.py
@@ -1,1 +1,20 @@
 #loop a list
+
+#loop through a list using a for loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#loop through a list using a while loop
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1  
+
+#loop through the index numbers of a list
+for i in range(len(fruits)):
+    print(fruits[i])
+
+#using list comprehension to loop through a list - it is a short hand for loop
+[print(fruit) for fruit in fruits]
+
