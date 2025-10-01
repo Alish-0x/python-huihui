44c6e072381bf829c7ead31cbf093d452578b093
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-01 20:33:35 +0545

    Add pythontuples.py to demonstrate tuple usage and properties in Python

---
+++ Day-3/pythontuples.py
@@ -0,0 +1,24 @@
+# Tuples are immutable sequences in Python
+my_tuple = (1, 2, 3, 'a', 'b', 'c')
+print(my_tuple)
+
+#its ordered,unchangeable, and allows duplicate values
+
+
+#length of a tuple - `len()`
+print(len(my_tuple))
+
+#creating tuple with one item
+single_item_tuple = (5,)
+print(single_item_tuple)
+
+#not a tuple
+not_a_tuple = (5)
+print(type(not_a_tuple))
+
+
+#tuple constructor `tuple()`
+my_tuple = tuple((1, 2, 3, 'a', 'b', 'c'))
+print(my_tuple)
+
+
