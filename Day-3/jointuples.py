c51006ce6208ee46f4c98bf872edfef1a2831562
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-01 20:46:38 +0545

    Add jointuples.py to demonstrate tuple joining and multiplication techniques in Python

---
+++ Day-3/jointuples.py
@@ -0,0 +1,11 @@
+# joining two tuples - `+` operator
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+# multiplying tuples - `*` operator
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)  
