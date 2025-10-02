aeb8645be5f328be491bbb56fa46c47ad67cbf29
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-02 20:33:53 +0545

    Add sets.py to demonstrate set creation and properties in Python

---
+++ Day-4/sets.py
@@ -0,0 +1,20 @@
+#sets - stores multiple items in a single variable
+
+# its unchangeable, unordered, and unindexed , dublicate not allowed 
+#its written with curly brackets
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+print(len(thisset))
+print(type(thisset))
+print("banana" in thisset)
+
+
+#`set()` constructor - to make a set
+thisset = set(("apple", "banana", "cherry")) # note the double round brackets
+print(thisset)
+
+
+
+
+
