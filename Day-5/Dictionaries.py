f0bab2f418561d50c735e7af83f6d03251f8c57c
HYPERXD <72203904+Alish-0x@users.noreply.github.com>
2025-10-03 21:31:49 +0545

    Add Dictionaries.py to demonstrate the creation and manipulation of dictionaries in Python

---
+++ Day-5/Dictionaries.py
@@ -0,0 +1,21 @@
+# Dictionaries - store data in key-value pairs - unordered, changeable, indexed
+
+#dictionary length
+my_dict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(len(my_dict))
+
+#`type` function
+print(type(my_dict))
+
+#`dict` constructor
+thisdict = dict(name="John", age=36, country="Norway")
+print(thisdict)
+
+
+
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
